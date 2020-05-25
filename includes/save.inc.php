375be2ca9c441aba78d3c02afbfb8cca5ea55672
JakovSudar <jakovsudar1@gmail.com>
2020-05-25 14:12:46 +0200

    uploads path fix

--- includes/save.inc.php
+++ includes/save.inc.php
@@ -11,7 +11,7 @@ $wins = $_POST["wins"];
 $loss = $_POST["loss"];
 
 //mjesto za spremanje slike
-$target_dir = "uploads/";
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
