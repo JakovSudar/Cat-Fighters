a645120393ef90aa980ef08be0e6561e24a05c28
JakovSudar <jakovsudar1@gmail.com>
2020-06-04 11:28:05 +0200

    more logs

--- includes/save.inc.php
+++ includes/save.inc.php
@@ -35,33 +35,34 @@ if(isset($_POST["submit"])) {
     echo"<br>";
     $uploadOk = 0;
   }
-//resizeanje slike na 300px
-$maxDim = 300;
-$file_name = $_FILES['fileToUpload']['tmp_name'];
-list($width, $height, $type, $attr) = getimagesize( $file_name );
-echo $width . $height . $type ;
-echo "<br>";
-echo $file_name; 
+  //resizeanje slike na 300px
+  $maxDim = 300;
+  $file_name = $_FILES['fileToUpload']['tmp_name'];
+  list($width, $height, $type, $attr) = getimagesize( $file_name );
+  echo "width height type: ";
+  echo $width . $height . $type ;
+  echo "<br>";
+  echo $file_name; 
 
-if ( $width > $maxDim || $height > $maxDim ) {
-    $target_filename = $file_name;
-    $ratio = $width/$height;
-    if( $ratio > 1) {
-        $new_width = $maxDim;
-        $new_height = $maxDim/$ratio;
-    } else {
-        $new_width = $maxDim*$ratio;
-        $new_height = $maxDim;
-    }
-    $src = imagecreatefromstring( file_get_contents( $file_name ) );
-    $dst = imagecreatetruecolor( $new_width, $new_height );
-    imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-    imagedestroy( $src );
-    imagepng( $dst, $target_filename ); // adjust format as needed
-    imagedestroy( $dst );
-    echo $target_filename;
-    echo "<br>";
-}   
+  if ( $width > $maxDim || $height > $maxDim ) {
+      $target_filename = $file_name;
+      $ratio = $width/$height;
+      if( $ratio > 1) {
+          $new_width = $maxDim;
+          $new_height = $maxDim/$ratio;
+      } else {
+          $new_width = $maxDim*$ratio;
+          $new_height = $maxDim;
+      }
+      $src = imagecreatefromstring( file_get_contents( $file_name ) );
+      $dst = imagecreatetruecolor( $new_width, $new_height );
+      imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+      imagedestroy( $src );
+      imagepng( $dst, $target_filename ); // adjust format as needed
+      imagedestroy( $dst );
+      echo $target_filename;
+      echo "<br>";
+  }   
   //Dopustanje samo odredenih fomata
   if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {    
     echo "wrong format!";
